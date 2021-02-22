cc7e6bca04bfe4197bcc35ebc20adfe9f32e048e
Daniel Haile <dannymom3@gmail.com>
2021-02-22 14:54:33 -0600

    init commit
    updated

---
+++ assignment1/demoCode.js
@@ -0,0 +1,6 @@
+'use strict';
+let firstName = "Daniel";
+let age = 28;
+let course = "cs301";
+console.log("This is " + firstName + " and I'm " + age + " years old, and currently, I'm taking " + course);
+//console.log(firstName);
